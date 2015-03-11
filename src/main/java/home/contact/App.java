021191566503c0032451cf29fd859e2961d3f388
PuntusovAleksandr <PuntusovAleksandr@gmail.com>
2015-03-12 02:25:16 +0300

    version 7 -probably the last option

--- src/main/java/home/contact/App.java
+++ src/main/java/home/contact/App.java
@@ -34,10 +34,11 @@ public class App {
         Contact contact = (Contact) context.getBean("Contact1");
         Contact contact1 =(Contact) context.getBean("Contact5");
 
-////        contactDao.addFriendShip(((Contact) context.getBean("Contact1")),((Contact) context.getBean("Contact5")));
+//        contactDao.addFriendShip(((Contact) context.getBean("Contact1")),((Contact) context.getBean("Contact5")));
 //        contactDao.addFriendShip(contact, contact1);
         javaContactService.createContact("Monja", "Figoc", LocalDate.ofEpochDay(2005-07-06));
-
+        javaContactService.createContact("Dunja", "Mirova", LocalDate.ofEpochDay(1885-07-06));
+        javaContactService.createContact("Вак", "Xprior", LocalDate.ofEpochDay(2020-07-06));
 
         System.out.println(contactDao.getContactFriendShipList());
         System.out.println("Contacts : "+contactDao.countContact());
@@ -46,7 +47,11 @@ public class App {
 
         javaContactService.addPlace("Little city","Nikoppol",65.568156, 45.4984 );
 
+        System.out.println("___________________________________________________");
+        contactDao.deleteContact((Contact) context.getBean("Contact3"));
+        System.out.println(contactDao.toString());
+        System.out.println("Contacts : "+contactDao.countContact());
+        System.out.println("___________________________________________________");
 
-        System.out.println();
     }
 }
