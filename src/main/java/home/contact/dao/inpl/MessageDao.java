681f3d89dfcfe288d2ab1cbb1f1fe98a3425a351
PuntusovAleksandr <PuntusovAleksandr@gmail.com>
2015-03-15 17:25:07 +0300

    delete mistake in to  ContactDao and HobbyDao and PlaceDao and MessageDao

--- src/main/java/home/contact/dao/inpl/MessageDao.java
+++ src/main/java/home/contact/dao/inpl/MessageDao.java
@@ -12,27 +12,23 @@ import java.util.List;
  * Created by Aleksandr on 07.03.2015.
  */
 public class MessageDao implements InterfaceMessageDao {
-    private List<MessageContact> messagesContacts;
-    private List<MessageContact> messageConversation;
+    private List<MessageContact> messagesContacts = new LinkedList<MessageContact>();
 
     @Override
     public void storeMessage(MessageContact message) {
-        if (message == null){
-            messagesContacts = new LinkedList<MessageContact>();
-        }
         messagesContacts.add(message);
     }
 
     @Override
     public List<MessageContact> getConversation(Contact contactOne, Contact contactTwo) {
+        List<MessageContact> messageConversation = new LinkedList<MessageContact>();
         if (messagesContacts==null) return null;
-        messageConversation = new LinkedList<MessageContact>();
         for (MessageContact mess : messagesContacts){
             if (mess.getFrom().equals(contactOne) && mess.getTo().equals(contactTwo) ||
                     mess.getFrom().equals(contactTwo) && mess.getTo().equals(contactOne)){
                 messageConversation.add(mess);
             }
         }
-        return messagesContacts;
+        return messageConversation;
     }
 }
